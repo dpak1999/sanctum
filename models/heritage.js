04703ce0d0d3cb01c94663a7ec0b728a3bf733eb
dpak1999 <deepakkumardash313@gmail.com>
2021-02-03 23:56:28 +0530

    deleting reviews on site delete

--- models/heritage.js
+++ models/heritage.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const HeritageSchema = new Schema({
@@ -15,6 +16,12 @@ const HeritageSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+HeritageSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
+  }
 });
 
 module.exports = mongoose.model("Heritage", HeritageSchema);
