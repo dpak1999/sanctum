2642339d16a001fa901a3adb71bbbc458e092179
dpak1999 <deepakkumardash313@gmail.com>
2021-02-08 22:52:28 +0530

    handling register form & saving user in db

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,27 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to sanctum");
+      res.redirect("/heritages");
+    } catch (e) {
+      req.flash("error", "Uh Oh! such an user already exists");
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
