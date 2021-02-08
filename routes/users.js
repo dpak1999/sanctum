bb4c497393afa091e942dac53e78f6fe3f8ceff5
dpak1999 <deepakkumardash313@gmail.com>
2021-02-08 22:59:28 +0530

    add : login routes & template

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/users");
 const catchAsync = require("../utils/catchAsync");
@@ -24,6 +25,22 @@ router.post(
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/heritages");
+  }
 );
 
 module.exports = router;
