968fb11e139615f17db648a04f209b32218748eb
dpak1999 <deepakkumardash313@gmail.com>
2021-03-20 19:14:36 +0530

    add : dotenv for environment vars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /** @format */
-
+if (process.env !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
