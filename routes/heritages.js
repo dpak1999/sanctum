1867c100362602cdb04364c9d4a462c7a3549474
dpak1999 <deepakkumardash313@gmail.com>
2021-03-20 19:02:42 +0530

    add : multer middleware

--- routes/heritages.js
+++ routes/heritages.js
@@ -5,11 +5,16 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateSite } = require("../middleware");
 const heritages = require("../controllers/heritages");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   .get(catchAsync(heritages.index))
-  .post(isLoggedIn, validateSite, catchAsync(heritages.createNewSite));
+  // .post(isLoggedIn, validateSite, catchAsync(heritages.createNewSite));
+  .post(upload.single("image"), (req, res) => {
+    console.log(req.body, req.file);
+  });
 
 router.get("/new", isLoggedIn, heritages.renderNewForm);
 
