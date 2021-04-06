5335133f064e6e644c5aa76b8a19deedafb420a0
dpak1999 <deepakkumardash313@gmail.com>
2021-04-07 03:59:31 +0530

    centering the map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -4,6 +4,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [-74.5, 40],
-  zoom: 4,
+  center: site.geometry.coordinates, //[lng,lat]
+  zoom: 8,
 });
+
+new mapboxgl.Marker().setLngLat(site.geometry.coordinates).addTo(map);
