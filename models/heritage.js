cb025b5617e0a331ad44d7a92955de769bd49413
dpak1999 <deepakkumardash313@gmail.com>
2021-04-20 11:44:29 +0530

    add : popup virtual in mongo & maps

--- models/heritage.js
+++ models/heritage.js
@@ -13,33 +13,44 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const HeritageSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const HeritageSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+HeritageSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/heritages/${this._id}">${this.title}</a></strong>
+  <p>${this.description.substring(0, 20)}...</p>
+  `;
 });
 
 HeritageSchema.post("findOneAndDelete", async function (doc) {
--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -4,8 +4,9 @@ mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/light-v10",
-  center: [-103.59179687498357, 40.66995747013945],
-  zoom: 3,
+  center: [3.113268, 39.323811],
+  // ,
+  zoom: 2,
 });
 
 map.on("load", function () {
@@ -94,15 +95,8 @@ map.on("load", function () {
   // the location of the feature, with
   // description HTML from its properties.
   map.on("click", "unclustered-point", function (e) {
+    const text = e.features[0].properties.popUpMarkup;
     var coordinates = e.features[0].geometry.coordinates.slice();
-    var mag = e.features[0].properties.mag;
-    var tsunami;
-
-    if (e.features[0].properties.tsunami === 1) {
-      tsunami = "yes";
-    } else {
-      tsunami = "no";
-    }
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -111,10 +105,7 @@ map.on("load", function () {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML("magnitude: " + mag + "<br>Was there a tsunami?: " + tsunami)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map);
   });
 
   map.on("mouseenter", "clusters", function () {
