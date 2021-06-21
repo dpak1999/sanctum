82e193696b03ff41980ada2a58aa1c6f4b5e161c
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-22 00:05:03 +0530

    fix error

--- app.js
+++ app.js
@@ -99,6 +99,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App listening");
 });
