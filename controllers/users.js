f14876be075dd7162e6d78bc3651823855cc2240
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-26 22:40:46 +0530

    update to resguide

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.register = async (req, res) => {
     const registeredUser = await User.register(user, password);
     req.login(registeredUser, (err) => {
       if (err) return next(err);
-      req.flash("success", "Welcome to sanctum");
+      req.flash("success", "Welcome to Resguide");
       res.redirect("/heritages");
     });
   } catch (e) {
